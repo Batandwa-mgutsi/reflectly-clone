317162a29efa58c4ca7d531fc29961d51c120118
Batandwa Mgutsi <B.mgutsi@gmail.com>
2020-11-13 11:58:56 +0200

    Add Flutter Driver

--- lib/profile_main.dart
+++ lib/profile_main.dart
@@ -3,12 +3,17 @@ import 'main.dart' as app;
 import 'package:testsweets/testsweets.dart';
 import 'app_automation_keys.dart';
 
+import 'package:flutter_driver/driver_extension.dart';
+
 void main() {
   syncAutomationKeys(
     projectId: 'LGLvnu3M8IMe5UO6CvZM',
     apiKey: '548c654d-d9ca-452e-9ee7-0340bc0b6375',
     automationKeys: APP_AUTOMATION_KEYS,
   );
+
+  // Enable flutter driver extension
+  enableFlutterDriverExtension();
 
   app.main();
 }
